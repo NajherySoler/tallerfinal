6f1fde904e1ef80d857991047fd784df3beb9bdb
Najhery Soler <najhery_02@hotmail.com>
2019-08-24 12:45:58 -0500

    primeroscambios

---
+++ GerreroZerg.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GerreroZerg here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GerreroZerg extends Zerg
+{
+    /**
+     * Act - do whatever the GerreroZerg wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
